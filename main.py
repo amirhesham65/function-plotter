ea34de9182d4b1fe2b6139dcc86a60f7c17abf51
Amir Hesham <amirhesham65@gmail.com>
2022-05-03 20:31:59 +0200

    fix bugs in gui + add error label and validation

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import tkinter as tk
-
-import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import (
     FigureCanvasTkAgg, NavigationToolbar2Tk)
-# Implement the default Matplotlib key bindings.
-from matplotlib.backend_bases import key_press_handler
 from matplotlib.figure import Figure
 from interpreter.lexer import Lexer
 from interpreter.parser_ import Parser
@@ -18,47 +14,13 @@ class GUIApp:
         self.root.minsize(400, 600)
         self.root.maxsize(400, 600)
         self.root.title("Function Plotter")
-        self.function_entry = None
-        self.min_entry = None
-        self.max_entry = None
-        self.graph_canvas = None
+        self.independent_values, self.dependent_values = [], []
 
-    def handle_button_click(self):
-        try:
-            function_string = self.function_entry.get()
-            min_val = float(self.min_entry.get()) if self.min_entry.get() else 0.0
-            max_val = float(self.max_entry.get()) if self.max_entry.get() else 0.0
-            self.plot_function(function_string, min_val, max_val)
-        except Exception as e:
-            print(e)
-
-    def plot_function(self, function_string: str, min_val: float, max_val: float):
-        if function_string:
-            try:
-                lexer = Lexer(function_string)
-                tokens = lexer.generate_tokens()
-                p = Parser(tokens)
-                tree = p.parse()
-                independent_values = list(range(int(min_val), int(max_val + 1), ))
-                dependent_values = [Interpreter(x).visit(tree).value for x in independent_values]
-                if self.graph_canvas is not None:
-                    self.graph_canvas.get_tk_widget().pack_forget()
-                fig = Figure(figsize=(5, 4), dpi=100)
-                a = fig.add_subplot()
-                a.plot(independent_values, dependent_values)
-                a.set_xlabel("x")
-                a.set_ylabel("f(x)")
-
-                self.graph_canvas = FigureCanvasTkAgg(fig, master=self.root)
-                self.graph_canvas.draw()
-                self.graph_canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
-            except Exception as e:
-                print(e)
-
-    def run(self):
+        # Headline
         label = tk.Label(master=self.root, text="Plot a function \nby filling the fields below", font=("Arial", 18))
         label.pack(pady=(15, 0))
 
+        # Inputs field
         top_frame = tk.Frame(self.root)
         top_frame.pack(pady=15, expand=True)
 
@@ -77,6 +39,15 @@ class GUIApp:
         self.max_entry = tk.Entry(master=top_frame)
         self.max_entry.grid(row=2, column=1)
 
+        # Plotting button
+        tk.Button(self.root, text="Plot the function", borderwidth=1, padx=5, pady=5,
+                  command=lambda: self.handle_button_click()).pack()
+
+        # Error label
+        self.error_message_label = tk.Label(master=self.root, text="", fg="#FF0000", highlightcolor="#FF0000")
+        self.error_message_label.pack()
+
+        # Function Plot
         initial_function = "x^2 + 4"
         initial_min = 400
         initial_max = 3000
@@ -85,12 +56,53 @@ class GUIApp:
         self.min_entry.insert(0, str(initial_min))
         self.max_entry.insert(0, str(initial_max))
 
-        tk.Button(self.root, text="Plot the function", borderwidth=1, padx=5, pady=5,
-                  command=lambda: self.handle_button_click()).pack()
+        self.fig = Figure(figsize=(5, 4), dpi=100)
+        self.ax = self.fig.add_subplot(111)
+        self.ax.plot(self.independent_values, self.dependent_values)
+        self.ax.set_xlabel("x")
+        self.ax.set_ylabel("f(x)")
+
+        graph_canvas = FigureCanvasTkAgg(self.fig, master=self.root)
+        graph_canvas.draw()
+        graph_canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=True)
         self.plot_function(initial_function, initial_min, initial_max)
+
         tk.mainloop()
+
+    def handle_error(self, message: str = ""):
+        self.error_message_label.config(text=message)
+
+    @staticmethod
+    def evaluate_function_string(function_string: str, min_val: float, max_val: float) -> (list, list):
+        lexer = Lexer(function_string)
+        tokens = lexer.generate_tokens()
+        p = Parser(tokens)
+        tree = p.parse()
+        independent_values = list(range(int(min_val), int(max_val + 1)))
+        dependent_values = [Interpreter(x).visit(tree).value for x in independent_values]
+        return independent_values, dependent_values
+
+    def handle_button_click(self):
+        self.handle_error()
+        try:
+            function_string = self.function_entry.get()
+            if not self.min_entry.get().isnumeric() or not self.max_entry.get().isnumeric():
+                raise Exception("Please enter a valid numeric range for minimum and maximum")
+            min_val = float(self.min_entry.get()) if self.min_entry.get() else 0.0
+            max_val = float(self.max_entry.get()) if self.max_entry.get() else 0.0
+            if min_val > max_val:
+                raise Exception("The minimum value should be less that the maximum")
+            self.plot_function(function_string, min_val, max_val)
+        except Exception as e:
+            self.handle_error(str(e))
+
+    def plot_function(self, function_string: str, min_val: float, max_val: float):
+        self.ax.clear()
+        self.independent_values, self.dependent_values = self.evaluate_function_string(function_string, min_val, max_val)
+        self.ax.plot(self.independent_values, self.dependent_values)
+        self.fig.canvas.draw()
 
 
 if __name__ == '__main__':
     app = GUIApp()
-    app.run()
+
